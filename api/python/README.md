85b55d4e188e61c51fb166b99cb814f7a793f169
Christopher Rogers <chrissrogers@gmail.com>
2014-05-29 11:36:01 -0700

    Adds python API example application
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

---
+++ api/python/README.md
@@ -0,0 +1,22 @@
+## API example: Python + Flask
+
+This small application demonstrates how you might set up a web server
+using Python and [Flask][flask] with RESTful routes to accept your Recurly.js
+form submissions and use the tokens to create and update customer billing
+information without having to handle credit card data.
+
+### Routes
+
+- `POST` [/subscriptions/new](app.py#L17-47)
+- `POST` [/accounts/new](app.py#L50-62)
+- `PUT` [/accounts/:account_code](app.py#L65-77)
+
+### Use
+
+```bash
+$ pip install Flask
+$ pip install Recurly
+$ python app.py
+```
+
+[flask]: http://flask.pocoo.org/
---
+++ api/python/app.py
@@ -0,0 +1,86 @@
+
+# Import Flask and recurly client library
+from flask import Flask
+from flask import request
+import recurly
+
+# We'll use uuid to generate unique account codes
+import uuid
+
+# Configure the recurly client with your subdomain and api key
+recurly.SUBDOMAIN = 'RECURLY_SUBDOMAIN'
+recurly.API_KEY = 'RECURLY_API_KEY'
+
+app = Flask(__name__)
+
+# POST route to handle a new subscription form
+@app.route("/subscriptions/new", methods=['POST'])
+def new_subscription():
+
+  # We'll wrap this in a try to catch any API
+  # errors that may occur
+  try:
+
+    # Create the scubscription using minimal
+    # information: plan_code, account_code, and
+    # the token we generated on the frontend
+    subscription = recurly.Subscription(
+      plan_code = 'basic',
+      account = recurly.Account(
+        account_code = uuid.uuid1(),
+        billing_info = recurly.BillingInfo(
+          token_id = request.form['recurly-token']
+        )
+      )
+    )
+
+    # The subscription has been created and we can redirect
+    # to a confirmation page
+    subscription.save
+    redirect('SUCCESS_URL')
+  except recurly.ValidationError, errors:
+
+    # Here we may wish to log the API error and send the
+    # customer to an appropriate URL, perhaps including
+    # and error message. See the `error_redirect` and
+    # `compose_errors` functions below.
+    error_redirect(compose_errors(errors))
+
+# POST route to handle a new account form
+@app.route("/accounts/new", methods=['POST'])
+def new_account():
+  try:
+    account = recurly.Account(
+      account_code = uuid.uuid1(),
+      billing_info = recurly.BillingInfo(
+        token_id = request.form['recurly-token']
+      )
+    )
+    account.save
+    redirect('SUCCESS_URL')
+  except recurly.ValidationError, errors:
+    error_redirect(compose_errors(errors))
+
+# PUT route to handle an account update form
+@app.route("/accounts/<account_code>", methods=['PUT'])
+def update_account(account_code):
+  try:
+    account = recurly.Account.get(account_code)
+    account.billing_info = recurly.BillingInfo(
+      token_id = request.form['recurly-token']
+    )
+    account.save
+    redirect('SUCCESS_URL')
+  except recurly.NotFoundError, error:
+    error_redirect(error.message)
+  except recurly.ValidationError, errors:
+    error_redirect(compose_errors(errors))
+
+def error_redirect(message):
+  redirect('ERROR_URL?errors=' + message)
+
+def compose_errors(errors):
+  ', '.join(e.message for e in errors)
+
+if __name__ == "__main__":
+  app.run()
