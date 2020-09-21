c9e53703ac49a862850080d4b1ac12e3e95c50d8
Christopher Rogers <chrissrogers@gmail.com>
2020-09-21 10:17:32 -0700

    Updates PHP example - Updates Recurly client to v3 - Updates Slim from v3 to v4 - Updates /api/subscription/new to use Purchase create endpoint - Updates README

--- api/php/app.php
+++ api/php/app.php
@@ -1,116 +1,163 @@
 <?php
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
 
 // This halts execution if a static file is found, allowing it to render instead
 if (PHP_SAPI == 'cli-server') {
   $url  = parse_url($_SERVER['REQUEST_URI']);
-  $file = __DIR__ . "/../../public/$url[path]";
+  $path = $url['path'] == '/' ? 'index.html' : $url['path'];
+  $file = __DIR__ . "/../../public/$path";
   if (is_file($file)) return false;
 }
 
 require 'vendor/autoload.php';
 
-// Configure the client with your subdomain and API Key. We're using ENV vars here,
+// Configure the client with your API Key. We're using ENV vars here,
 // but you may wish to store them elsewhere
-Recurly_Client::$subdomain = $_ENV['RECURLY_SUBDOMAIN'];
-Recurly_Client::$apiKey = $_ENV['RECURLY_API_KEY'];
+$recurly_client = new \Recurly\Client($_ENV['RECURLY_API_KEY']);
 
-$app = new \Slim\App;
+$app = AppFactory::create();
 
 // Create a new account, subscription, and billing information
+// For this we will use the Recurly Create Purchase endpoint
+// See: https://developers.recurly.com/api/latest/index.html#operation/create_purchase
 $app->post('/api/subscriptions/new', function (Request $request, Response $response, array $args) {
+
+  global $recurly_client;
+
+  // Retrieve the parsed body from the request as params
+  $params = (array)$request->getParsedBody();
+
+  // Retrieve the token created by Recurly.js and submitted in our form
+  $token_id = $params['recurly-token'];
+
+  // If our form specifies an account code, we can use that; otherwise,
+  // create an account code with a uniqid
+  $account_code = $params['recurly-account-code'];
+  if (is_null($account_code)) {
+    $account_code = uniqid();
+  }
+
+  // Specify the minimum purchase attributes for a subscription: plan_code, account, and currency
+  $purchase_create = [
+    'currency' => 'USD',
+    'account' => [
+      'code' => $account_code,
+      'first_name' => $params['first-name'],
+      'last_name' => $params['last-name'],
+      'billing_info' => [
+        'token_id' => $token_id
+      ],
+    ],
+    'subscriptions' => [
+      [
+        'plan_code' => 'basic'
+      ]
+    ]
+  ];
+
+  // Optionally add a 3D Secure token if one is present
+  $three_d_secure_token = $params['three-d-secure-token'];
+  if ($three_d_secure_token) {
+    $purchase_create['account']['billing_info']['three_d_secure_action_result_token_id'] = $three_d_secure_token;
+  }
+
   // We wrap this is a try-catch to handle any errors
   try {
 
-    $tokenId = $request->getParam('recurly-token');
+    // Create the purchase
+    $recurly_client->createPurchase($purchase_create);
 
-    // Specify the minimum subscription attributes: plan_code, account, and currency
-    $subscription = new Recurly_Subscription();
-    $subscription->plan_code = 'basic';
-    $subscription->currency = 'USD';
-
-    // Create an account with a uniqid and the customer's first and last name
-    $account_code = $request->getParam('recurly-account-code');
-    if (is_null($account_code)) {
-      $account_code  = uniqid();
-    }
-    $subscription->account = new Recurly_Account($account_code);
-    $subscription->account->first_name = $request->getParam('first-name');
-    $subscription->account->last_name = $request->getParam('last-name');
-
-    // Now we create a bare BillingInfo with a token
-    $subscription->account->billing_info = new Recurly_BillingInfo();
-    $subscription->account->billing_info->token_id = $tokenId;
-
-    // Optionally add a 3D Secure token if one is present
-    if ($request->getParam('three-d-secure-token')) {
-      $subscription->account->billing_info->three_d_secure_action_result_token_id = $request->getParam('three-d-secure-token');
-    }
-
-    // Create the subscription
-    $subscription->create();
-
-  } catch (Exception $e) {
+  } catch (\Recurly\Errors\Transaction $e) {
 
     // Here we handle a 3D Secure required error by redirecting to an authentication page
-    if ($e->errors[0] && $e->errors[0]->error_code == 'three_d_secure_action_required') {
-      $actionTokenId = $e->errors[0]->three_d_secure_action_token_id;
-      return $response->withRedirect("/3d-secure/authenticate.html#token_id=$tokenId&action_token_id=$actionTokenId&account_code=$account_code");
+    $transaction_error = $e->getApiError()->getTransactionError();
+    if ($transaction_error && $transaction_error->getCode() == 'three_d_secure_action_required') {
+      $action_token_id = $transaction_error->getThreeDSecureActionTokenId();
+      $location = "/3d-secure/authenticate.html#token_id=$token_id&action_token_id=$action_token_id&account_code=$account_code";
+      return $response->withHeader('Location', $location)->withStatus(302);
     }
 
     // Assign the error message and use it to handle any customer messages or logging
     $error = $e->getMessage();
+
+  } catch (\Recurly\Errors\Validation $e) {
+
+    // If the request was not valid, you may want to tell your user why.
+    $error = $e->getMessage();
+
   }
 
-  // Now we may wish to redirect to a confirmation
-  // or back to the form to fix errors.
+  // Now we may wish to redirect to a confirmation or back to the form to fix any errors.
+  $location = $_ENV['SUCCESS_URL'];
   if (isset($error)) {
-    return $response->withRedirect("$_ENV[ERROR_URL]?error=$error");
-  } else {
-    return $response->withRedirect("$_ENV[SUCCESS_URL]");
+    $location = "$_ENV[ERROR_URL]?error=$error";
   }
+
+  return $response->withHeader('Location', $location)->withStatus(302);
 });
 
 // Create a new account and billing information
 $app->post('/api/accounts/new', function (Request $request, Response $response, array $args) {
+  global $recurly_client;
+
+  $params = (array)$request->getParsedBody();
+
+  $account_create = [
+    'code' => $account_code,
+    'first_name' => $params['first-name'],
+    'last_name' => $params['last-name'],
+    'billing_info' => [
+      'token_id' => $params['recurly-token']
+    ]
+  ];
+
   try {
-    $account = new Recurly_Account(uniqid());
-    $account->billing_info = new Recurly_BillingInfo();
-    $account->billing_info->token_id = $request->getParam('recurly-token');
-    $account->create();
-  } catch (Exception $e) {
+    $recurly_client->createAccount($account_create);
+  } catch (\Recurly\Errors\Validation $e) {
     $error = $e->getMessage();
   }
-  if ($error) {
-    return $response->withRedirect("$_ENV[ERROR_URL]?error=$error");
-  } else {
-    return $response->withRedirect("$_ENV[SUCCESS_URL]");
+
+  $location = $_ENV['SUCCESS_URL'];
+  if (isset($error)) {
+    $location = "$_ENV[ERROR_URL]?error=$error";
   }
+
+  return $response->withHeader('Location', $location)->withStatus(302);
 });
 
 $app->put('/api/accounts/{account_code}', function (Request $request, Response $response, array $args) {
+  global $recurly_client;
+
+  $params = (array)$request->getParsedBody();
+
+  $account_update = [
+    'first_name' => $params['first-name'],
+    'last_name' => $params['last-name'],
+    'billing_info' => [
+      'token_id' => $params['recurly-token']
+    ]
+  ];
+
   try {
-    $account = Recurly_Account::get($args['account_code']);
-    $account->first_name = $request->getParam('first-name');
-    $account->last_name = $request->getParam('last-name');
-    $account->billing_info = new Recurly_BillingInfo();
-    $account->billing_info->token_id = $request->getParam('recurly-token');
-    $account->update();
-  } catch (Exception $e) {
+    $recurly_client->updateAccount("code-$args[account_code]", $account_update);
+  } catch (\Recurly\Errors\Validation $e) {
     $error = $e->getMessage();
   }
-  if ($error) {
-    return $response->withRedirect("$_ENV[ERROR_URL]?error=$error");
-  } else {
-    return $response->withRedirect("$_ENV[SUCCESS_URLs]");
+
+  $location = $_ENV['SUCCESS_URL'];
+  if (isset($error)) {
+    $location = "$_ENV[ERROR_URL]?error=$error";
   }
+
+  return $response->withHeader('Location', $location)->withStatus(302);
 });
 
 // This endpoint provides configuration to recurly.js
 $app->get('/config', function (Request $request, Response $response, array $args) {
   $response->getBody()->write("window.recurlyConfig = { publicKey: '$_ENV[RECURLY_PUBLIC_KEY]' }");
-  return $response;
+  return $response->withHeader('Content-Type', 'application/javascript');
 });
 
 $app->run();
