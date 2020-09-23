4426b979bf67efe628ae8dcef3ffad96d1b8eb79
Dave Brudner <dbrudne1@gmail.com>
2020-09-23 12:53:09 -0500

    Updates node example
    - Updates Recurly client to v3
    - Updates /api/subscription/new to use Purchase create endpoint
    - Updates README

--- api/node/app.js
+++ api/node/app.js
@@ -1,5 +1,5 @@
 // API usage Dependencies
-const Recurly = require('node-recurly');
+const recurly = require('recurly')
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -22,18 +22,13 @@ const {
 } = process.env;
 
 // Instantiate a configured recurly client
-const recurly = new Recurly({
-  SUBDOMAIN: RECURLY_SUBDOMAIN,
-  API_KEY: RECURLY_API_KEY,
-  API_VERSION: '2.21'
-});
+const client = new recurly.Client(RECURLY_API_KEY)
 
 // POST route to handle a new subscription form
-app.post('/api/subscriptions/new', function (req, res) {
-
+app.post('/api/subscriptions/new', async function (req, res) {
   // Build our billing info hash
   const tokenId = req.body['recurly-token'];
-  const accountCode = req.body['recurly-account-code'] || uuid.v1();
+  const code = req.body['recurly-account-code'] || uuid.v1();
   const billingInfo = { token_id: tokenId };
 
   // Optionally add a 3D Secure token if one is present. You only need to do this
@@ -42,57 +37,69 @@ app.post('/api/subscriptions/new', function (req, res) {
     billingInfo.three_d_secure_action_result_token_id = req.body['three-d-secure-token']
   }
 
-  // Create the scubscription using minimal
-  // information: plan_code, currency, account_code, and
+  // Create the purchase using minimal
+  // information: planCode, currency, account.code, and
   // the token we generated on the frontend
-  recurly.subscriptions.create({
-    plan_code: 'basic',
+  const purchaseReq = {
+    subscriptions: [{ planCode: 'basic' }],
     currency: 'USD',
-    account: {
-      account_code: accountCode,
-      billing_info: billingInfo
-    }
-  }, function (err, response) {
-    if (err) {
-      // Here we handle a 3D Secure required error by redirecting to an authentication page
-      if (err.data.errors && err.data.errors.transaction_error) {
-        const txnError = err.data.errors.transaction_error;
-        if (txnError.error_code === 'three_d_secure_action_required') {
-          const actionTokenId = txnError.three_d_secure_action_token_id;
-          return res.redirect(`/3d-secure/authenticate.html#token_id=${tokenId}&action_token_id=${actionTokenId}&account_code=${accountCode}`);
-        }
-      }
+    account: { code, billingInfo }
+  }
 
-      // If any other error occurs, parse the error message
-      // and redirect to an error page
-      const message = errorMessage(err.data);
-      return res.redirect(ERROR_URL + '?error=' + message);
-    }
-
-    // Otherwise, we know that the request has succeeded,
-    // and we can redirect to a confirmation page
+  try {
+    await client.createPurchase(purchaseReq);
     res.redirect(SUCCESS_URL);
-  });
+  }
+  catch (err) {
+    // Here we handle a 3D Secure required error by redirecting to an authentication page
+    if (err && err.transactionError && err.transactionError.code === 'three_d_secure_action_required') {
+      const { threeDSecureActionTokenId } = err.transactionError;
+      const url = `/3d-secure/authenticate.html#token_id=${tokenId}&action_token_id=${threeDSecureActionTokenId}&account_code=${code}`
 
+      return res.redirect(url);
+    }
+
+    // If any other error occurs, redirect to an error page with the error as a query param
+    const { message } = err;
+    return res.redirect(`${ERROR_URL}?error=${message}`);
+  }
 });
 
 // POST route to handle a new account form
-app.post('/api/accounts/new', function (req, res) {
-  recurly.accounts.create({
-    account_code: uuid.v1(),
-    billing_info: {
-      token_id: req.body['recurly-token']
+app.post('/api/accounts/new', async function (req, res) {
+  try {
+    const accountCreate = {
+      code: uuid.v1(),
+      billing_info: {
+        token_id: req.body['recurly-token']
+      }
     }
-  }, redirect);
+
+    await client.createAccount(accountCreate);
+    res.redirect(SUCCESS_URL);
+  }
+  catch (err) {
+    const { message } = err;
+    return res.redirect(`${ERROR_URL}?error=${message}`);
+  }
 });
 
 // PUT route to handle an account update form
-app.put('/api/accounts/:account_code', function (req, res) {
-  recurly.accounts.update(req.params.account_code, {
-    billing_info: {
-      token_id: req.body['recurly-token']
+app.put('/api/accounts/:account_code', async function (req, res) {
+  try {
+    const accountUpdate = {
+      billing_info: {
+        token_id: req.body['recurly-token']
+      }
     }
-  }, redirect);
+
+    await client.updateAccount(req.params.account_code, accountUpdate);
+    res.redirect(SUCCESS_URL);
+  }
+  catch (err) {
+    const { message } = err;
+    return res.redirect(`${ERROR_URL}?error=${message}`);
+  }
 });
 
 // This endpoint provides configuration to recurly.js
@@ -108,26 +115,3 @@ app.use(express.static(__dirname + '/../../public'));
 app.listen(9001, function () {
   console.log('Listening on port 9001');
 });
-
-// A set of utility functions for redirecting and parsing API errors
-function redirect (err, response) {
-  if (err) return res.redirect(`${ERROR_URL}?error=${errorMessage(err.data)}`);
-  res.redirect(SUCCESS_URL);
-}
-
-// Simple error parsing routine to construct a helpful error message
-function errorMessage (data) {
-  if (data.errors) {
-    return parseValidationErrors(data.errors.error);
-  }
-
-  console.log(data);
-
-  if (data.error.symbol) {
-    return [data.error.symbol, data.error.description].join(': ');
-  }
-}
-
-function parseValidationErrors (e) {
-  return [e.$.field, e._].join(' ');
-}
